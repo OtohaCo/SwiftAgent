c473ebd8766f37c464fd53fbb68e1948936533f0
Long Li <lilong@gmail.com>
2026-09-19 17:44:54 +0900

    Fix provider tests on Linux

--- Tests/AgentProvidersTests/ResponsesContinuationIntegrityTests.swift
+++ Tests/AgentProvidersTests/ResponsesContinuationIntegrityTests.swift
@@ -2,6 +2,9 @@ import AgentCore
 import AgentModels
 import AgentTools
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 import Testing
 @testable import AgentProviders
 
